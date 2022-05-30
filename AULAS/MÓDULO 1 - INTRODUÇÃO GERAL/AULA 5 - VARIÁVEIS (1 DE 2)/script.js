a791193d546fe078e83ef0f239c8bd278708d35c
Peretta <viniassisperetta@gmail.com>
2022-05-30 19:33:23 -0300

    Váriaveis (1/2) concluída!

---
+++ AULAS/MÓDULO 1 - INTRODUÇÃO GERAL/AULA 5 - VARIÁVEIS (1 DE 2)/script.js
@@ -0,0 +1,43 @@
+//EXEMPLO 1//
+
+var nomeCompleto = 'Vinicius Assis Peretta';
+var idade = 90;
+
+alert(nomeCompleto + ' possui ' + idade);
+
+//EXEMPLO 2//
+
+//A//
+
+var x = 10;
+var y = 15;
+
+var total = x + y;
+
+alert(total);
+
+//EXEMPLO 3//
+
+var nome = "Vinicius"
+var sobrenome = "Peretta"
+
+var nomecompleto = nome + ' '+ sobrenome;//concatenar
+
+alert(nomecompleto);
+
+//EXEMPLO 4//
+
+alert(x+nome);//Ambos strings agora!
+
+//EXEMPLO 5//
+
+//A//
+
+var x = '10';
+var y = '15';
+
+var total = x + y;
+
+alert(total);
+
+
